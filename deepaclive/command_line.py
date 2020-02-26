b8f513f4d84c9377cc1b5dd1f14daf3f74d62838
JakubBartoszewicz <kuba.bartoszewicz@gmail.com>
2020-02-26 14:30:23 +0100

    cli

--- deepaclive/command_line.py
+++ deepaclive/command_line.py
@@ -1,31 +1,93 @@
 import sklearn # to load libgomp early to solve problems with static TLS on some systems like bioconda mulled-tests
 from deepaclive.receiver import Receiver
 from deepaclive.sender import Sender
+import argparse
+from deepaclive import __version__
 
 
 def main():
-    cycles = [50, 100, 158, 208]
-    read_length = 100
-    in_dir = "out"
-    mid_dir = "dlmid"
-    out_dir = "dlout"
     # TODO: asynch
-    run_local("deepacvir", "rapid", read_length, in_dir, mid_dir, out_dir, cycles)
+    parse()
 
 
-def run_sender(read_length, in_dir, out_dir, cycles):
-    sender = Sender(read_length=read_length, input_dir=in_dir, output_dir=out_dir)
+def run_sender(args):
+    sender = Sender(read_length=args.read_length, input_dir=args.in_dir, output_dir=args.send_out_dir)
+    cycles = [int(c) for c in args.cycle_list.split(',')]
     sender.run(cycles)
 
 
-def run_receiver(deepac_command, model, read_length, in_dir, out_dir, cycles):
-    receiver = Receiver(deepac_command, model=model, read_length=read_length, input_dir=in_dir, output_dir=out_dir)
+def run_receiver(args):
+    receiver = Receiver(args.command, model=args.model, read_length=args.read_length, input_dir=args.rec_in_dir,
+                        output_dir=args.rec_out_dir)
+    cycles = [int(c) for c in args.cycle_list.split(',')]
     receiver.run(cycles)
 
 
-def run_local(deepac_command, model, read_length, in_dir, mid_dir, out_dir, cycles):
-    run_sender(read_length, in_dir, mid_dir, cycles)
-    run_receiver(deepac_command, model, read_length, mid_dir, out_dir, cycles)
+def run_local(args):
+    run_sender(args)
+    run_receiver(args)
+
+
+def add_base_parser(bparser):
+    bparser.add_argument('-l', '--read-length', dest='read_length', type=int, required=True,
+                         help='Expected read length')
+
+    bparser.add_argument('-s', '--seq-cycles', dest='cycle_list', required=True,
+                         help='Comma-separated list of sequencing cycles to analyze.')
+    return bparser
+
+
+def add_receiver_parser(rparser):
+    command_group = rparser.add_mutually_exclusive_group()
+    command_group.add_argument('-c', '--command', default='deepac', help='DeePaC command to use '
+                                                                         '(switches builtin models).')
+    command_group.add_argument('-C', '--custom', action='store_true', help='Use a custom model.')
+    rparser.add_argument('-m', '--model', default='rapid',  help='Model to use. "rapid", "sensitive" '
+                                                                 'or custom .h5 file.')
+
+    rparser.add_argument('-I', '--receiver-input', dest='rec_in_dir', required=True)
+    rparser.add_argument('-O', '--receiver-output', dest='rec_out_dir', required=True)
+    return rparser
+
+
+def add_sender_parser(sparser):
+    sparser.add_argument('-i', '--sender-input', dest='in_dir', required=True)
+    sparser.add_argument('-o', '--sender-output', dest='send_out_dir', required=True)
+    return sparser
+
+
+def parse():
+    """Parse DeePaC-live CLI arguments."""
+    parser = argparse.ArgumentParser(prog='deepac-live', description="Running DeePaC in real time.")
+    parser.add_argument('-v', '--version', dest='version', action='store_true', help='Print version.')
+
+    subparsers = parser.add_subparsers(help='DeePaC-live subcommands. See command --help for details.',
+                                       dest='subparser')
+    parser_sender = subparsers.add_parser('sender', help='Prepare and send data.')
+    parser_sender = add_base_parser(parser_sender)
+    parser_sender = add_sender_parser(parser_sender)
+    parser_sender.set_defaults(func=run_sender)
+
+    parser_receiver = subparsers.add_parser('receiver', help='Receive and analyze data.')
+    parser_receiver = add_base_parser(parser_receiver)
+    parser_receiver = add_receiver_parser(parser_receiver)
+    parser_receiver.set_defaults(func=run_receiver)
+
+    parser_local = subparsers.add_parser('local', help='Process data locally.')
+    parser_local = add_base_parser(parser_local)
+    parser_local = add_receiver_parser(parser_local)
+    parser_local = add_sender_parser(parser_local)
+    parser_local.set_defaults(func=run_local)
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+    elif hasattr(args, 'func'):
+        args.func(args)
+    else:
+        print(__version__)
+        parser.print_help()
 
 
 if __name__ == "__main__":
